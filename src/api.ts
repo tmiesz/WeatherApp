aadcda882c3b0bb9926e9974372cbba1ef4d40d4
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-19 15:03:36 +0200

    extend api to fetch 2 days data

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { weatherSchema } from "./schemas/weatherSchema";
 
 export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
   const res = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code&hourly=temperature_2m,weather_code&forecast_days=1&daily=temperature_2m_mean,temperature_2m_min,temperature_2m_max,weather_code`,
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code&hourly=temperature_2m,weather_code&forecast_days=2&daily=temperature_2m_mean,temperature_2m_min,temperature_2m_max,weather_code`,
   );
   const data = await res.json();
 
