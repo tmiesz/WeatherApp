a7926e753484862b5c9e59ae7ac7a14228fbdbee
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-19 14:58:35 +0200

    fix api call

--- src/schemas/weatherSchema.ts
+++ src/schemas/weatherSchema.ts
@@ -1,23 +1,53 @@
 import { z } from "zod";
 
 export const weatherSchema = z.object({
+  latitude: z.number(),
+  longitude: z.number(),
+  generationtime_ms: z.number(),
+  utc_offset_seconds: z.number(),
+  timezone: z.string(),
+  timezone_abbreviation: z.string(),
+  elevation: z.number(),
+
+  current_units: z.object({
+    time: z.string(),
+    interval: z.string(),
+    temperature_2m: z.string(),
+    weather_code: z.string(),
+  }),
+
   current: z.object({
     time: z.string(),
+    interval: z.number(),
     temperature_2m: z.number(),
-    relative_humidity_2m: z.number(),
-    wind_speed_10m: z.number(),
     weather_code: z.number(),
   }),
+
+  hourly_units: z.object({
+    time: z.string(),
+    temperature_2m: z.string(),
+    weather_code: z.string(),
+  }),
+
   hourly: z.object({
-    time: z.array(z.string()),
+    time: z.array(z.string()), // ISO datetime[]
     temperature_2m: z.array(z.number()),
     weather_code: z.array(z.number()),
-    wind_speed_10m: z.array(z.number()),
   }),
+
+  daily_units: z.object({
+    time: z.string(),
+    temperature_2m_mean: z.string(),
+    temperature_2m_min: z.string(),
+    temperature_2m_max: z.string(),
+    weather_code: z.string(),
+  }),
+
   daily: z.object({
     time: z.array(z.string()),
-    temperature_2m_max: z.array(z.number()),
+    temperature_2m_mean: z.array(z.number()),
     temperature_2m_min: z.array(z.number()),
+    temperature_2m_max: z.array(z.number()),
     weather_code: z.array(z.number()),
   }),
 });
