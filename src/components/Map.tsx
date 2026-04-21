1ad21742e10ad03ff5d622983cbaaf2f2339e913
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-21 22:04:33 +0200

    map panning

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 export default function Map() {
@@ -8,6 +8,7 @@ export default function Map() {
       zoom={5}
       style={{ width: "1000px", height: "500px" }}
     >
+      <MapClick />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -15,4 +16,15 @@ export default function Map() {
       <Marker position={[50, 50]} />
     </MapContainer>
   );
+}
+
+function MapClick() {
+  const map = useMap();
+
+  map.on("click", (e) => {
+    const { lat, lng } = e.latlng;
+    map.panTo([lat, lng]);
+  });
+
+  return null;
 }
