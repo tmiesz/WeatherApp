6d4f572a0bb868c8c14ed954240f7d2bc497f4fe
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-21 18:03:54 +0200

    map template

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import DailyForecast from "./components/cards/DailyForecast";
 import HourlyForecast from "./components/cards/HourlyForecast";
 import CurrentWeather from "./components/cards/CurrentWeather";
 import AdditionalInfo from "./components/cards/AdditionalInfo";
+import Map from "./components/Map";
 
 function App() {
   return (
     <div className="flex flex-col gap-8">
+      <Map />
       <CurrentWeather />
       <HourlyForecast />
       <DailyForecast />
---
+++ src/components/Map.tsx
@@ -0,0 +1,18 @@
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function Map() {
+  return (
+    <MapContainer
+      center={[50, 50]}
+      zoom={5}
+      style={{ width: "1000px", height: "500px" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[50, 50]} />
+    </MapContainer>
+  );
+}
