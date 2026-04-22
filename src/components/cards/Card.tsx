8f3440382f995c63deb7acb547e8616239501c2d
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-22 17:51:14 +0200

    card styling

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function Card({ children, title, childrenClassName }: Props) {
   return (
-    <div className="p-4 rounded-xl bg-zinc-900 shadow-md flex flex-col gap-4">
+    <div className="p-4 rounded-xl  bg-linear-to-br from-zinc-900 to-zinc-900/60 shadow-md flex flex-col gap-4">
       <h2 className="text-2xl font-semibold">{title}</h2>
       <div className={childrenClassName}>{children}</div>
     </div>
